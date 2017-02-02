9bf511655317ccfc27eb3d223bbed45aa72d8613
Mohanarangan Muthukumar <mohan@extrasalt.org>
2017-02-02 22:35:43 +0530

    parse form data and check password against hardcoded inputs

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main 
 
 import (
-    "net/http"
-    "time"
-    "github.com/gorilla/mux"
-    "github.com/dgrijalva/jwt-go"
+"net/http"
+"time"
+"github.com/gorilla/mux"
+"github.com/dgrijalva/jwt-go"
+"fmt"
 )
 
 var secret = []byte("secrety") //get this from os.env
@@ -23,15 +24,32 @@ func main(){
 
 func GetTokenHandler(w http.ResponseWriter, r *http.Request){
 
-    token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+
+    err := r.ParseForm()
+
+    if err != nil {
+        panic(err)
+    }
+
+    username := r.Form["name"][0]
+    password := r.Form["password"][0]   
+
+    fmt.Println(username, password)
+
+    if username == "mohan" && password == "meme" {
+
+        token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
             "username" : "mohan",
             "exp" : time.Now().Add(time.Hour * 24).Unix(),
-        })
+            })
 
-    tokenString, _ := token.SignedString(secret)
+        tokenString, _ := token.SignedString(secret)
 
-    cookie := &http.Cookie{Name: "jwtcookie", Value: tokenString, MaxAge: 3600, Secure: false, HttpOnly: true, Raw: tokenString}
-    http.SetCookie(w, cookie)
-    
-    w.Write([]byte(tokenString))
+        cookie := &http.Cookie{Name: "jwtcookie", Value: tokenString, MaxAge: 3600, Secure: false, HttpOnly: true, Raw: tokenString}
+        http.SetCookie(w, cookie)
+
+        w.Write([]byte(tokenString))
+    } else {
+        w.Write([]byte("Auth failed"))
+    }
 }
