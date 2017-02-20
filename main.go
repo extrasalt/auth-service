e44bc3fab0746bab6f097deeac8f72b1b96dcd6e
Mohanarangan Muthukumar <mohan@extrasalt.org>
2017-02-20 12:40:54 +0530

    Ran gofmt on main.go

--- main.go
+++ main.go
@@ -1,60 +1,57 @@
-package main 
+package main
 
 import (
-"net/http"
-"time"
-"github.com/gorilla/mux"
-"github.com/dgrijalva/jwt-go"
-"fmt"
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gorilla/mux"
+	"net/http"
+	"time"
 )
 
 var secret = []byte("secrety") //get this from os.env
 
-func main(){
-    r := mux.NewRouter()
+func main() {
+	r := mux.NewRouter()
 
-    r.HandleFunc("/auth", GetTokenHandler).Methods("POST")
+	r.HandleFunc("/auth", GetTokenHandler).Methods("POST")
 
-    r.PathPrefix("/").Handler(http.FileServer(http.Dir("./static/")))
+	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./static/")))
 
-    
-
-    http.ListenAndServe(":3000", r)
+	http.ListenAndServe(":3000", r)
 }
 
-func GetTokenHandler(w http.ResponseWriter, r *http.Request){
+func GetTokenHandler(w http.ResponseWriter, r *http.Request) {
 
+	err := r.ParseForm()
 
-    err := r.ParseForm()
+	if err != nil {
+		panic(err)
+	}
 
-    if err != nil {
-        panic(err)
-    }
+	username := r.Form["name"][0]
+	password := r.Form["password"][0]
+	redirectUrl := r.Form["redirect"]
 
-    username := r.Form["name"][0]
-    password := r.Form["password"][0]
-    redirectUrl := r.Form["redirect"]   
+	fmt.Println(username, password)
 
-    fmt.Println(username, password)
+	if username == "mohan" && password == "meme" {
 
-    if username == "mohan" && password == "meme" {
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+			"username": "mohan",
+			"exp":      time.Now().Add(time.Hour * 24).Unix(),
+		})
 
-        token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-            "username" : "mohan",
-            "exp" : time.Now().Add(time.Hour * 24).Unix(),
-            })
+		tokenString, _ := token.SignedString(secret)
 
-        tokenString, _ := token.SignedString(secret)
+		cookie := &http.Cookie{Name: "jwtcookie", Value: tokenString, MaxAge: 3600, Secure: false, HttpOnly: true, Raw: tokenString}
+		http.SetCookie(w, cookie)
 
-        cookie := &http.Cookie{Name: "jwtcookie", Value: tokenString, MaxAge: 3600, Secure: false, HttpOnly: true, Raw: tokenString}
-        http.SetCookie(w, cookie)
-
-        if redirectUrl != nil {
-            http.Redirect(w,r,redirectUrl[0],302)
-        } else {
-            w.Write([]byte(tokenString))
-        }
-    } else {
-        w.Write([]byte("Auth failed"))
-    }
+		if redirectUrl != nil {
+			http.Redirect(w, r, redirectUrl[0], 302)
+		} else {
+			w.Write([]byte(tokenString))
+		}
+	} else {
+		w.Write([]byte("Auth failed"))
+	}
 }
